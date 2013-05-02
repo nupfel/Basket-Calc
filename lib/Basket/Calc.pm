dcc77a03f69398d2fef16c6c59ca23e7e02154ba
Tobias Kirschstein <lev@ideegeo.com>
2013-05-02 14:15:36 +1200

    initial release

---
+++ lib/Basket/Calc.pm
@@ -0,0 +1,366 @@
+package Basket::Calc;
+
+use 5.010;
+use Mouse;
+use Scalar::Util qw(looks_like_number);
+use Finance::Currency::Convert::Yahoo;
+use Carp;
+use feature 'switch';
+
+=head1 NAME
+
+Basket::Calc - Basket/Cart calculation library with support for currency conversion, discounts and tax
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Basket::Calc;
+    use Data::Dump 'dump';
+
+    my $basket = Basket::Calc->new(debug => 1, currency => 'NZD', tax => .15);
+    $basket->add_item({ price => 14.90, currency => 'USD', quantity => 2 });
+    $basket->add_item({ price => 59, currency => 'EUR'});
+    $basket->add_item({ price => 119.15, currency => 'JPY' });
+    
+    $basket->add_discount({ type => 'percent', value => .2 });
+    # or
+    $basket->add_discount({ type => 'amount', value => 15, currency => 'HKD' });
+    
+    print dump $basket->calculate;
+
+=head1 ATTRIBUTES
+
+=head2 debug
+
+=cut
+
+has 'debug' => (
+    is      => 'rw',
+    isa     => 'Bool',
+    trigger => \&_set_debug,
+    lazy    => 1,
+    default => sub { 0 },
+);
+
+has 'items' => (
+    is      => 'rw',
+    isa     => 'ArrayRef',
+    clearer => 'empty_items',
+);
+
+has 'discount' => (
+    is      => 'rw',
+    isa     => 'HashRef',
+    clearer => 'no_discount',
+);
+
+=head2 currency
+
+=cut
+
+has 'currency' => (
+    is       => 'rw',
+    isa      => 'Str',
+    required => 1,
+);
+
+=head2 tax
+
+=cut
+
+has 'tax' => (
+    is      => 'rw',
+    isa     => 'Num',
+    lazy    => 1,
+    default => sub { 0 },
+);
+
+=head1 SUBROUTINES/METHODS
+
+=head2 add_item
+
+=cut
+
+sub add_item {
+    my ($self, $item) = @_;
+
+    # make sure the input is sane
+    unless (ref $item eq 'HASH') {
+        carp "parameter has to be a HASHREF";
+        return;
+    }
+
+    foreach my $key ('price') {
+        unless (exists $item->{$key} and $item->{$key}) {
+            carp "$key missing";
+            return;
+        }
+    }
+
+    unless (looks_like_number($item->{price})) {
+        carp "'price' is not a number";
+        return;
+    }
+
+    # calculate amount from quantity and price
+    if (exists $item->{quantity}) {
+        unless (looks_like_number($item->{quantity}) and $item->{quantity} > 0)
+        {
+            carp "'quantity' is not a number or smaller than 0";
+            return;
+        }
+
+        $item->{amount} = $item->{price} * $item->{quantity};
+    }
+    else {
+        $item->{amount} = $item->{price};
+    }
+
+    $item->{currency} = $self->currency
+        unless (exists $item->{currency} and $item->{currency});
+
+    # convert currency if needed
+    if ($item->{currency} ne $self->currency) {
+        my $amount =
+            Finance::Currency::Convert::Yahoo::convert($item->{amount},
+            $item->{currency}, $self->currency);
+
+        unless ($amount) {
+            carp "could not get "
+                . $item->{amount} . " "
+                . $item->{currency}
+                . " converted to "
+                . $self->currency;
+            return;
+        }
+
+        $item->{orig_amount}   = $item->{amount};
+        $item->{orig_currency} = $item->{currency};
+
+        $item->{amount}   = $amount;
+        $item->{currency} = $self->currency;
+    }
+
+    print __PACKAGE__ . ' added item: ' . join(' ', %$item) . $/
+        if $self->debug;
+
+    $self->items([ @{ $self->items || [] }, $item ]);
+
+    return $self->items;
+}
+
+=head2 add_discount
+
+=cut
+
+sub add_discount {
+    my ($self, $discount) = @_;
+
+    # make sure the input is sane
+    unless (ref $discount eq 'HASH') {
+        carp "parameter has to be a HASHREF";
+        return;
+    }
+
+    foreach my $key ('type', 'value') {
+        unless (exists $discount->{$key} and $discount->{$key}) {
+            carp "'$key' missing";
+            return;
+        }
+    }
+
+    unless ($discount->{type} =~ m/^(percent|amount)$/) {
+        carp "'type' has to be either percent, or amount";
+        return;
+    }
+
+    unless (looks_like_number($discount->{value})) {
+        carp "'value' is not a number";
+        return;
+    }
+
+    given ($discount->{type}) {
+        when ('percent') {
+            if ($discount->{value} <= 0 or $discount->{value} > 1) {
+                carp "'percent' has to be a decimal between 0 and 1";
+                return;
+            }
+        }
+        when ('amount') {
+            $discount->{currency} = $self->currency
+                unless exists $discount->{currency};
+
+            # convert currency if needed
+            if ($discount->{currency} ne $self->currency) {
+                my $amount = Finance::Currency::Convert::Yahoo::convert(
+                    $discount->{value}, $discount->{currency}, $self->currency);
+
+                unless ($amount) {
+                    carp "could not get "
+                        . $discount->{value} . " "
+                        . $discount->{currency}
+                        . " converted to "
+                        . $self->currency;
+                    return;
+                }
+
+                $discount->{orig_value}    = $discount->{value};
+                $discount->{orig_currency} = $discount->{currency};
+
+                $discount->{value}    = $amount;
+                $discount->{currency} = $self->currency;
+            }
+        }
+    }
+
+    print __PACKAGE__ . ' added discount: ' . join(' ', %$discount) . $/
+        if $self->debug;
+
+    $self->discount($discount);
+
+    return $self->discount;
+}
+
+=head2 calculate
+
+=cut
+
+sub calculate {
+    my ($self) = @_;
+
+    my $total = {
+        value      => 0,
+        net        => 0,
+        tax_amount => 0,
+        discount   => 0,
+    };
+
+    print __PACKAGE__ . " -- calculating totals --\n";
+
+    # calculate net
+    foreach my $item (@{ $self->items }) {
+        print __PACKAGE__ . ' item: ' . join(' ', %$item) . $/ if $self->debug;
+
+        $total->{net} += $item->{amount};
+    }
+
+    my $original_net = $total->{net};
+
+    # apply discounts
+    if ($self->discount) {
+        print __PACKAGE__ . ' discount: ' . join(' ', %{ $self->discount }) . $/
+            if $self->debug;
+
+        given ($self->discount->{type}) {
+            when ('percent') {
+                $total->{net} *= (1 - $self->discount->{value});
+            }
+            when ('amount') {
+                $total->{net} = $total->{net} - $self->discount->{value};
+                $total->{net} = 0 if $total->{net} < 0;
+            }
+        }
+    }
+
+    # calculate tax
+    $total->{tax_amount} = $total->{net} * $self->tax;
+    $total->{value}      = $total->{net} + $total->{tax_amount};
+    $total->{discount}   = $original_net - $total->{net};
+
+    # proper rounding and formatting
+    $total->{$_} = _round($total->{$_}) for keys %$total;
+
+    # remind what the currency is that was requested
+    $total->{currency} = $self->currency;
+
+    print __PACKAGE__ . ' total: ' . join(' ', %$total) . $/ if $self->debug;
+
+    $self->empty_items;
+    $self->no_discount;
+    $self->tax(0);
+
+    return $total;
+}
+
+sub _set_debug {
+    my ($self, $value, $some) = @_;
+
+    $Finance::Currency::Convert::Yahoo::CHAT = $value;
+}
+
+sub _round {
+    my ($float) = @_;
+
+    return sprintf('%.2f', sprintf('%.10f', $float)) + 0;
+}
+
+=head1 AUTHOR
+
+Tobias Kirschstein, C<< <lev at cpan.org> >>
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-basket-calc at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Basket-Calc>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Basket::Calc
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Basket-Calc>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Basket-Calc>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Basket-Calc>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Basket-Calc/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Tobias Kirschstein.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1;    # End of Basket::Calc
